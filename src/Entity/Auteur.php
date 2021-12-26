35d1d7df91a8aff47ef58948230011823577b14d
Ayoub <ubmagh@gmail.com>
2021-12-27 00:54:15 +0100

    created Entities

---
+++ src/Entity/Auteur.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AuteurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AuteurRepository::class)
+ */
+class Auteur
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nom_prenom;
+
+    /**
+     * @ORM\Column(type="string", length=1)
+     */
+    private $sexe;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_de_naissance;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nationalite;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomPrenom(): ?string
+    {
+        return $this->nom_prenom;
+    }
+
+    public function setNomPrenom(string $nom_prenom): self
+    {
+        $this->nom_prenom = $nom_prenom;
+
+        return $this;
+    }
+
+    public function getSexe(): ?string
+    {
+        return $this->sexe;
+    }
+
+    public function setSexe(string $sexe): self
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+    public function getDateDeNaissance(): ?\DateTimeInterface
+    {
+        return $this->date_de_naissance;
+    }
+
+    public function setDateDeNaissance(\DateTimeInterface $date_de_naissance): self
+    {
+        $this->date_de_naissance = $date_de_naissance;
+
+        return $this;
+    }
+
+    public function getNationalite(): ?string
+    {
+        return $this->nationalite;
+    }
+
+    public function setNationalite(string $nationalite): self
+    {
+        $this->nationalite = $nationalite;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Livre.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LivreRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=LivreRepository::class)
+ */
+class Livre
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=17)
+     */
+    private $isbn;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $nombre_pages;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_de_parution;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $note;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIsbn(): ?string
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn(string $isbn): self
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getNombrePages(): ?string
+    {
+        return $this->nombre_pages;
+    }
+
+    public function setNombrePages(string $nombre_pages): self
+    {
+        $this->nombre_pages = $nombre_pages;
+
+        return $this;
+    }
+
+    public function getDateDeParution(): ?\DateTimeInterface
+    {
+        return $this->date_de_parution;
+    }
+
+    public function setDateDeParution(\DateTimeInterface $date_de_parution): self
+    {
+        $this->date_de_parution = $date_de_parution;
+
+        return $this;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(int $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+}
