7850a28d512a317dafa60d7dcb5ac8eeb936b945
Ayoub <ubmagh@gmail.com>
2021-12-27 11:50:22 +0100

    Generate crud pages from entities

---
+++ src/Form/LivreType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Livre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LivreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('isbn')
+            ->add('titre')
+            ->add('nombre_pages')
+            ->add('date_de_parution')
+            ->add('note')
+            ->add('auteurs')
+            ->add('genres')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Livre::class,
+        ]);
+    }
+}
